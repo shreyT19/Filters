9c1e6a1532cdc763097056d616fdb29dddf277cf
Shreyansh Tripathy <shreyanshtripathy49@gmail.com>
2025-04-16 07:45:40 +0530

    feat: add keyboard accessibility to ApplyFilterButton for Enter key press

--- src/components/global/Filter/components/ApplyFilterButton.tsx
+++ src/components/global/Filter/components/ApplyFilterButton.tsx
@@ -1,16 +1,20 @@
+"use client";
+import { useEffect } from "react";
 import { Button, type ButtonProps } from "~/components/ui/button";
-import { FILTER_V2_TEST_IDS } from "~/utils/filter.utils";
 
 const ApplyFilterButton = (props: ButtonProps) => {
-  //   useKeyboardShortcut(
-  //     "Enter",
-  //     (e) => {
-  //       if (!props.disabled) {
-  //         props?.onClick?.(e as unknown as React.MouseEvent<HTMLButtonElement>);
-  //       }
-  //     },
-  //     true
-  //   );
+  useEffect(() => {
+    const handleEnterPress = (e: KeyboardEvent) => {
+      if (e.key === "Enter" && !props.disabled) {
+        props?.onClick?.(e as unknown as React.MouseEvent<HTMLButtonElement>);
+      }
+    };
+
+    window.addEventListener("keydown", handleEnterPress);
+    return () => {
+      window.removeEventListener("keydown", handleEnterPress);
+    };
+  }, [props.disabled, props.onClick]);
 
   return (
     <Button
